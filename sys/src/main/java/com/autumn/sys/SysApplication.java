0d9ffdc8c66c3bc8baa983b6d4deea521c950ad6
ruanxy <ruanxiangyu1995>
2021-10-22 17:10:26 +0800

    Initial commit

---
+++ sys/src/main/java/com/autumn/sys/SysApplication.java
@@ -0,0 +1,17 @@
+package com.autumn.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ruanxy
+ * @Date 2021-10-22
+ */
+@SpringBootApplication
+public class SysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysApplication.class, args);
+    }
+
+}
